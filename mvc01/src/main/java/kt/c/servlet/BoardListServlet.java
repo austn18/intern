74945c33148f4b9472d6478146abb82ed92586e2
austn18 <austn18@gmail.com>
2014-08-06 22:06:09 +0900

    .

--- mvc01/src/main/java/kt/c/servlet/BoardListServlet.java
+++ mvc01/src/main/java/kt/c/servlet/BoardListServlet.java
@@ -13,20 +13,16 @@ import kr.co.kt.board.db.BoardDAO;
 
 @WebServlet("/board/list")
 public class BoardListServlet extends HttpServlet {
-	@Override
+	@Override 
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 	    throws ServletException, IOException {
 		
-		// 1. BoardDAO 도구를 준비
 		BoardDAO dao = new BoardDAO();
 		
-		// 2. BoardDAO 도구를 사용해서 게시물 목록을 가져옴.
 		req.setAttribute("list", dao.selectAll());
 		
-		// 3. JSP로 화면 처리를 위임하기 위해 요청 배달자를 준비
 		RequestDispatcher rd = req.getRequestDispatcher("/view/board/list.jsp");
 		
-		// 4. 요청 배달자에게 요청 배달을 요구함.
 		rd.forward(req, resp);
 		
 		
